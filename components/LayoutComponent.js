abcbc5bac2eddc97e55d4a30c9f57a635ed6bade
nayzakcreations <nayzakcreations@gmail.com>
2021-07-28 17:12:57 -0500

    First

--- components/LayoutComponent.js
+++ components/LayoutComponent.js
@@ -21,6 +21,12 @@ const LayoutComponent = ({children}) => {
     if (!hidden) {
         return (
             <>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                          rel="stylesheet"/>
+                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                          rel="stylesheet"/>
+                </Head>
                 <div className={classes.root}>
                     <NavBarComponent/>
                     <Toolbar/>

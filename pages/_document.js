5bc0ef7b6f9f413eb2dc0756b7484ad90b560147
nayzakcreations <nayzakcreations@gmail.com>
2021-07-28 17:15:40 -0500

    First

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+import Script from 'next/script'
+import React from "react";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return {...initialProps}
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    {/*<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"*/}
+                    {/*      rel="stylesheet"/>*/}
+                    {/*<link href="https://fonts.googleapis.com/icon?family=Material+Icons"*/}
+                    {/*      rel="stylesheet"/>*/}
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                    <Script src="https://cdn.jsdelivr.net/npm/eosjs-api@7.0.4/lib/eos-api.min.js"/>
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument

f0006cc9638747ea8f1c939b04e5781a3cd4c23b
nayzakcreations <nayzakcreations@gmail.com>
2021-07-28 09:02:29 -0500

    First

--- components/LayoutComponent.js
+++ components/LayoutComponent.js
@@ -18,30 +18,34 @@ const LayoutComponent = ({children}) => {
     useEffect(() => {
         setHidden(false)
     }, [setHidden])
-    return (
-        <>
-            <Head>
-                <link rel="stylesheet"
-                      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-                {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-                <script src="https://cdn.jsdelivr.net/npm/eosjs-api@7.0.4/lib/eos-api.min.js"/>
-            </Head>
-            <div className={classes.root}>
-                <NavBarComponent/>
-                <Toolbar/>
-                <Container>
-                    <Box my={2}>
-                        {children}
-                    </Box>
-                </Container>
-            </div>
-            <SnackbarComponent/>
-            <RecaptchaFieldComponent/>
-            <DialogComponent/>
-        </>
-    )
+    if (!hidden) {
+        return (
+            <>
+                <Head>
+                    <link rel="stylesheet"
+                          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                    {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+                    <script src="https://cdn.jsdelivr.net/npm/eosjs-api@7.0.4/lib/eos-api.min.js"/>
+                </Head>
+                <div className={classes.root}>
+                    <NavBarComponent/>
+                    <Toolbar/>
+                    <Container>
+                        <Box my={2}>
+                            {children}
+                        </Box>
+                    </Container>
+                </div>
+                <SnackbarComponent/>
+                <RecaptchaFieldComponent/>
+                <DialogComponent/>
+            </>
+        )
+    } else {
+        return ''
+    }
 }
 
 export default LayoutComponent

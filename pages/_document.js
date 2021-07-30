8d37d4aa2fd71493c8e0f7b59f26355471539438
nayzakcreations <nayzakcreations@gmail.com>
2021-07-29 19:33:38 -0500

    First

--- pages/_document.js
+++ pages/_document.js
@@ -12,10 +12,8 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/*<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"*/}
-                    {/*      rel="stylesheet"/>*/}
-                    {/*<link href="https://fonts.googleapis.com/icon?family=Material+Icons"*/}
-                    {/*      rel="stylesheet"/>*/}
+                    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                          rel="stylesheet"/>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
                     <Script src="https://cdn.jsdelivr.net/npm/eosjs-api@7.0.4/lib/eos-api.min.js"/>
                 </Head>

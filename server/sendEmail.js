3ee2250a859bc5f750e1915b70a3dd326581b485
nayzakcreations <nayzakcreations@gmail.com>
2021-08-07 00:11:04 -0500

    First

--- server/sendEmail.js
+++ server/sendEmail.js
@@ -2,7 +2,7 @@ var nodemailer = require('nodemailer');
 
 export async function sendEmail(emailAddress, subject, message) {
 
-    var transporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
         port: 465,
         host: "smtp.gmail.com",
         auth: {
@@ -12,13 +12,56 @@ export async function sendEmail(emailAddress, subject, message) {
         secure: true
     });
 
-    const mailOptions = {
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
+
+    const mailData = {
         from: 'BlockCommerc <blockcommerc@gmail.com>', // sender address
         to: emailAddress, // list of receivers
         subject: subject, // Subject line
         html: message// plain text body
     };
 
-    await transporter.sendMail(mailOptions, function (err, info) {
-    })
+    await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailData, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve(info);
+            }
+        });
+    });
+
+    // var transporter = nodemailer.createTransport({
+    //     port: 465,
+    //     host: "smtp.gmail.com",
+    //     auth: {
+    //         user: 'blockcommerc@gmail.com',
+    //         pass: 'Snapshooter1'
+    //     },
+    //     secure: true
+    // });
+    //
+    // const mailOptions = {
+    //     from: 'BlockCommerc <blockcommerc@gmail.com>', // sender address
+    //     to: emailAddress, // list of receivers
+    //     subject: subject, // Subject line
+    //     html: message// plain text body
+    // };
+    //
+    // await transporter.sendMail(mailOptions, function (err, info) {
+    // })
 }

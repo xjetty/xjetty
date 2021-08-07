f8f382922a3bd3e2d56bf7d09a0e0f7a2ed16b2a
nayzakcreations <nayzakcreations@gmail.com>
2021-08-07 00:42:45 -0500

    First

--- server/sendEmail.js
+++ server/sendEmail.js
@@ -1,17 +1,27 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 export async function sendEmail(emailAddress, subject, message) {
 
-    // const transporter = nodemailer.createTransport({
-    //     port: 465,
-    //     host: "smtp.gmail.com",
-    //     auth: {
-    //         user: 'blockcommerc@gmail.com',
-    //         pass: 'Snapshooter1'
-    //     },
-    //     secure: true
-    // });
-    //
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+            user: 'blockcommerc@gmail.com',
+            pass: 'Snapshooter1'
+        },
+        secure: true
+    });
+
+    try {
+        await transporter.verify(function (error, success) {
+            if (error) {
+                return false
+            }
+        })
+    } catch (e) {
+        return false
+    }
+
     // await new Promise((resolve, reject) => {
     //     // verify connection configuration
     //     transporter.verify(function (error, success) {
@@ -24,14 +34,22 @@ export async function sendEmail(emailAddress, subject, message) {
     //         }
     //     });
     // });
-    //
-    // const mailData = {
-    //     from: 'BlockCommerc <blockcommerc@gmail.com>', // sender address
-    //     to: emailAddress, // list of receivers
-    //     subject: subject, // Subject line
-    //     html: message// plain text body
-    // };
-    //
+
+    const mailData = {
+        from: 'BlockCommerc <blockcommerc@gmail.com>', // sender address
+        to: emailAddress, // list of receivers
+        subject: subject, // Subject line
+        html: message// plain text body
+    };
+
+    try {
+        await transporter.sendMail(mailData, (err, info) => {
+            return !err
+        })
+    } catch (e) {
+        return false
+    }
+
     // await new Promise((resolve, reject) => {
     //     // send mail
     //     transporter.sendMail(mailData, (err, info) => {
@@ -45,23 +63,23 @@ export async function sendEmail(emailAddress, subject, message) {
     //     });
     // });
 
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-            user: 'blockcommerc@gmail.com',
-            pass: 'Snapshooter1'
-        },
-        secure: true
-    });
-
-    const mailOptions = {
-        from: 'BlockCommerc <blockcommerc@gmail.com>',
-        to: emailAddress,
-        subject: subject,
-        html: message
-    };
-
-    await transporter.sendMail(mailOptions, function (err, info) {
-    })
+    // var transporter = nodemailer.createTransport({
+    //     port: 465,
+    //     host: "smtp.gmail.com",
+    //     auth: {
+    //         user: 'blockcommerc@gmail.com',
+    //         pass: 'Snapshooter1'
+    //     },
+    //     secure: true
+    // });
+    //
+    // const mailOptions = {
+    //     from: 'BlockCommerc <blockcommerc@gmail.com>', // sender address
+    //     to: emailAddress, // list of receivers
+    //     subject: subject, // Subject line
+    //     html: message// plain text body
+    // };
+    //
+    // await transporter.sendMail(mailOptions, function (err, info) {
+    // })
 }

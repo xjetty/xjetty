6545311f6de24809d9b4df3554ece6c3beff9752
nayzakcreations <nayzakcreations@gmail.com>
2021-08-09 17:31:00 -0500

    First

--- pages/api/buyItNow.js
+++ pages/api/buyItNow.js
@@ -32,7 +32,7 @@ const buyItNow = async (req, res) => {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
     })
-    return res.json({success: false, number: 'asdfsadf'.replaceAll('a', '')})
+    return res.json({success: false, number: 'asdfsadf'.replace('/a/g', '')})
     const method = req.method
     if (method === 'POST') {
         const data = req.body

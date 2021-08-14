399122d84cc7e9c70d4aa98fc3114ea9c1862c04
nayzakcreations <nayzakcreations@gmail.com>
2021-08-14 10:06:00 -0500

    First

--- pages/index.js
+++ pages/index.js
@@ -157,7 +157,7 @@ const Home = () => {
                 </Grid>
                 <Grid item xs={12}>
                     <Card>
-                        <List dense>
+                        <List>
                             <ListItem>
                                 <ListItemIcon>
                                     <Copyright/>

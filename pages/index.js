c267a6903d6b9939cba55d3b08534146a937dd08
nayzakcreations <nayzakcreations@gmail.com>
2021-08-14 00:50:48 -0500

    First

--- pages/index.js
+++ pages/index.js
@@ -155,7 +155,7 @@ const Home = () => {
                         </CardActions>
                     </Card>
                 </Grid>
-                <Grid item xs={12} style={{textAlign: 'center'}}>
+                <Grid item xs={12}>
                     <Typography variant="body2">
                         Copyright &#169; { new Date().getFullYear() } BlockCommerc. All Rights Reserved
                     </Typography>

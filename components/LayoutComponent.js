6f9a8027bf63fb0548d49baa3fbd4b8679c403fc
nayzakcreations <nayzakcreations@gmail.com>
2021-08-14 00:43:25 -0500

    First

--- components/LayoutComponent.js
+++ components/LayoutComponent.js
@@ -1,36 +1,20 @@
 import NavBarComponent from "./NavBarComponent";
-import {Box, Container, makeStyles, Toolbar} from "@material-ui/core";
+import {Box, Container, Toolbar} from "@material-ui/core";
 import SnackbarComponent from "./SnackbarComponent";
 import RecaptchaFieldComponent from "./FieldComponents/RecaptchaFieldComponent";
 import React from "react";
 import DialogComponent from "./DialogComponent";
-import FooterComponent from "./FooterComponent";
-
-const useStyles = makeStyles(() => ({
-    root: {
-        // flexGrow: 1
-    },
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh'
-    }
-}))
 
 const LayoutComponent = ({children}) => {
-    const classes = useStyles()
     return (
         <>
-            <div className={classes.root}>
-                <NavBarComponent/>
-                <Toolbar/>
-                <Container className={classes.container}>
-                    <Box my={2}>
-                        {children}
-                    </Box>
-                </Container>
-                <FooterComponent/>
-            </div>
+            <NavBarComponent/>
+            <Toolbar/>
+            <Container>
+                <Box my={2}>
+                    {children}
+                </Box>
+            </Container>
             <SnackbarComponent/>
             <RecaptchaFieldComponent/>
             <DialogComponent/>

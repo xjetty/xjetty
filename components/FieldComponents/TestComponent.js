5f77b5cb7e64725467eb513189782527b6688101
nayzakcreations <nayzakcreations@gmail.com>
2021-08-21 00:13:49 -0500

    First

--- components/FieldComponents/TestComponent.js
+++ components/FieldComponents/TestComponent.js
@@ -1,7 +1,8 @@
-import {useContext, useState} from "react"
+import {useContext, useEffect, useMemo} from "react"
 import {Grid, IconButton, InputAdornment, TextField} from "@material-ui/core"
 import {Add, Remove} from "@material-ui/icons"
-import {AppContext} from "../../contexts/AppContext";
+import {AppContext} from "../../contexts/AppContext"
+const urlExists = require('url-exists')
 
 function TestComponent() {
     const {inputList, setInputList} = useContext(AppContext)
@@ -23,6 +24,15 @@ function TestComponent() {
         setInputList([...inputList, ''])
     }
 
+    const hasError = useMemo((x) => {
+        if (x.trim()) {
+            urlExists(x.trim(), function (err, exists) {
+                return !exists
+            })
+        } else
+            return false
+    }, [inputList])
+
     return (
         <>
             {inputList.map((x, i) => {
@@ -30,6 +40,7 @@ function TestComponent() {
                     <Grid item xs={12} key={i}>
                         <TextField
                             value={x}
+                            error={hasError(x)}
                             onChange={e => handleInputChange(e, i)}
                             fullWidth
                             label={`Image link ${i + 1}`}

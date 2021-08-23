be8a72c51fe71aaadbba32d6b84ed0795b74b44f
nayzakcreations <nayzakcreations@gmail.com>
2021-08-23 11:47:46 -0500

    First

--- components/FieldComponents/KeywordsFieldComponent.js
+++ components/FieldComponents/KeywordsFieldComponent.js
@@ -1,33 +1,24 @@
 import {TextField} from '@material-ui/core'
 import {useContext} from 'react'
 import {AppContext} from '../../contexts/AppContext'
-import ChipInput from 'material-ui-chip-input'
 
 const KeywordsFieldComponent = () => {
     const {keywords, setKeywords} = useContext(AppContext)
 
-    const handleAddChip = (chip) => {
-        const keywords = [...keywords, chip]
-        setKeywords(keywords)
-    }
-
-    const handleDeleteChip = (chip, index) => {
-        const keywords = [...keywords]
-        keywords.splice(index, 1)
-        setKeywords(keywords)
+    const handle = (event) => {
+        let value = event.target.value
+        setKeywords(value)
     }
 
     return (
-        <ChipInput
-            fullWidth
-            inputLabelProps={{required: true}}
+        <TextField
+            placeholder="Use commas to separate keywords"
             value={keywords}
-            alwaysShowPlaceholder
-            placeholder="Press enter after every keyword"
-            onAdd={(chip) => handleAddChip(chip)}
-            onDelete={(chip, index) => handleDeleteChip(chip, index)}
-            variant="filled"
+            onChange={handle}
+            fullWidth
             label="Keywords"
+            variant="filled"
+            multiline
         />
     )
 }

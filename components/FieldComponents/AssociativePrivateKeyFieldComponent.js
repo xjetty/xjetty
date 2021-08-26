781d9d5f50cd7f4fff62a374094d48d56e93b4e4
nayzakcreations <nayzakcreations@gmail.com>
2021-08-26 07:10:09 -0500

    First

--- components/FieldComponents/AssociativePrivateKeyFieldComponent.js
+++ components/FieldComponents/AssociativePrivateKeyFieldComponent.js
@@ -34,6 +34,7 @@ const AssociativePrivateKeyFieldComponent = () => {
 
     return (
         <TextField
+            InputLabelProps={{required: true}}
             error={associativePrivateKeyError}
             helperText={helperText}
             onBlur={checkError}

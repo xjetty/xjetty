6dc138b9dbc38f4137bb77718c460d3ccf42694b
nayzakcreations <nayzakcreations@gmail.com>
2021-09-04 21:55:33 -0500

    First

--- pages/api/getListings.js
+++ pages/api/getListings.js
@@ -31,7 +31,6 @@ const getListings = async (req, res) => {
             worldwide = cookie.parse(req.headers.worldwide)
             countries = cookie.parse(req.headers.countries)
         }
-        const listingsPerPage = 64
         let listings = []
         if (worldwide) {
             if (search.trim()) {
@@ -43,14 +42,14 @@ const getListings = async (req, res) => {
                     $or: [{title: {'$regex': search, '$options': 'i'}},
                         {description: {'$regex': search, '$options': 'i'}},
                         {keywords: {'$regex': search, '$options': 'i'}}]
-                }).skip().limit(listingsPerPage)
+                })
             } else {
                 listings = await Listing.find({
                     hidden: false,
                     code: {$ne: null},
                     publicListing: true,
                     worldwide: true
-                }).skip().limit(listingsPerPage)
+                })
             }
         } else {
             if (search.trim()) {
@@ -62,14 +61,14 @@ const getListings = async (req, res) => {
                     $or: [{title: {'$regex': search, '$options': 'i'}},
                         {description: {'$regex': search, '$options': 'i'}},
                         {keywords: {'$regex': search, '$options': 'i'}}]
-                }).skip().limit(listingsPerPage)
+                })
             } else {
                 listings = await Listing.find({
                     hidden: false,
                     code: {$ne: null},
                     publicListing: true,
                     countries: {$in: countries}
-                }).skip().limit(listingsPerPage)
+                })
             }
         }
 
